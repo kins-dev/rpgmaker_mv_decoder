2e5507eed9d5e64150ff09ade8cf59ca6ee9478d
Scott Atkins <scott@kins.dev>
2022-04-18 08:24:07 -0700

    Bump version: 1.1.0 → 1.1.1

--- setup.py
+++ setup.py
@@ -51,6 +51,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/kins-dev/rpgmaker_mv_decoder",
-    version="1.1.0",
+    version="1.1.1",
     zip_safe=False,
 )
